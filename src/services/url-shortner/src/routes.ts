1c93f64ce9bc86f19abddfc300f8d7d80591d98f
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-09-26 22:27:15 +0530

    chore: base structure init

---
+++ src/services/url-shortner/src/routes.ts
@@ -0,0 +1,18 @@
+import { Application, Request, Response } from "express"
+
+export interface IAppRoutes {
+    app: Application
+}
+
+export const initRoutes = ({ app }: IAppRoutes) => {
+    
+    // Add hb routes
+    app.get('/hb', (_: Request, res: Response) => {
+        res.status(200).json({
+            code: 0,
+            message: 'Server alive!'
+        })
+    })
+
+    // TODO: Add Error handlers (May be middleware)
+}
