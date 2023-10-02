f1fc945b2c0701c43d5267db33de7abab67514ed
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-10-02 11:38:15 +0530

    refactored main file

--- src/services/url-shortner/src/main.ts
+++ src/services/url-shortner/src/main.ts
@@ -1,16 +1,25 @@
 import { initApp } from './app';
 import { initRoutes } from './routes';
+import { logger } from './utils';
 
-(async function () {
+function bootstrap() {
   //TODO: DB Conn
 
   //TODO: Redis conn
 
   // create an express application.
-  const app = await initApp({
+  const app = initApp({
     port: 5555,
   });
 
   // Routes
   initRoutes({ app });
+}
+
+(function () {
+  try {
+    bootstrap();
+  } catch (e) {
+    logger.fatal('Failed to bootstrap the `url-shortner` service: ', e);
+  }
 })();
